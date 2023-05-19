c3d23154caf912cbe8fef1186e162aafd56ef0b6
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-19 18:28:01 +0530

    commit

--- src/ageCalculator.ts
+++ src/ageCalculator.ts
@@ -1,16 +1,52 @@
 
+const hasHadBirthday=(current:Date,prev:Date):boolean=>{
+    
+    if(current.getMonth()>prev.getMonth()){
+      return true;
+    }
+
+    if(current.getMonth()===prev.getMonth() && current.getDate()>=prev.getDate()){
+       return true;
+    }
+
+    return false;
+
+}
+function isLeapYear(date: Date): boolean {
+  const year = date.getFullYear();
+
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+const isLeapDate=(birth:Date)=>{
+   return(isLeapYear(birth) && birth.getDate()===29)
+}
 
 interface propTypes{
   
-  calculate:(from:Date,to:Date)=>number
+  calculateDifference:(current:Date,prev:Date)=>number
 }
 export const ageCalculator=():propTypes=>{
   
   return{
-      calculate:(from:Date,to:Date):number=>{
-        const year1 = from.getFullYear();
-        const year2 = to.getFullYear();
-        return Math.abs(year2 - year1);
+      calculateDifference:(current:Date,prev:Date):number=>{
+        
+        let age=Math.abs(current.getFullYear()-prev.getFullYear())
+
+        if(isLeapDate(prev))age=Math.floor(age/4)
+        
+        if(!hasHadBirthday(current,prev))
+            age-=1
+            
+        return age;
       }
 }
 
--- tests/ageCalculator.test.ts
+++ tests/ageCalculator.test.ts
@@ -1,26 +1,97 @@
 import { ageCalculator } from "../src/ageCalculator";
 
+// 4 solution boundries : 
+// 1.already had b'day 
+// 2.b'day yet to come
+// 3.b'day on leap year 
+// 4.b'day on the same day as fromDate
 describe('ageCalculator', () => { 
 
-  describe('date range', () => {
+  describe('already had birthday this year', () => {
   const testCases = [
-    { fromDate: new Date(), toDate: new Date('1985-03-04'),expected:38 },
-    { fromDate: new Date(), toDate: new Date('1989-02-16'),expected:34 },
-    { fromDate: new Date(), toDate: new Date('1958-03-31'),expected:65 },
+    { current: new Date(), prev: new Date('1985-03-04'),expected:38 },
+    { current: new Date(), prev: new Date('1989-02-16'),expected:34 },
+    { current: new Date(), prev: new Date('1958-03-31'),expected:65 },
   ];
 
   test.each(testCases)(
-    'should calculate difference between $fromDate - $toDate to be $expected',
-    ({ fromDate, toDate,expected }) => {
+    'should calculate difference between $current - $prev to be $expected',
+    ({ current, prev,expected }) => {
       // your test code here
      //Assemble
        const sut=ageCalculator()
       //Act 
-       const acutual =sut.calculate(fromDate,toDate)
+       const acutual =sut.calculateDifference(current,prev)
        
       //Assert
        expect(acutual).toBe(expected)
     }
   );
 });
+
+  describe('b`day should be a leap year', () => {
+    const testCases = [
+      { current:new Date() , prev: new Date('1980-02-29'), expected: 10 },
+      { current:new Date() , prev: new Date('1976-02-29'), expected: 11 },
+      { current:new Date() , prev: new Date('1972-02-29'), expected: 12 },
+    ];
+
+    test.each(testCases)(
+      'should calculate age difference between $current and $prev to be $expected',
+      ({ current, prev, expected }) => {
+
+        // Assemble
+        const sut = ageCalculator();
+
+        // Act
+        const actual = sut.calculateDifference(current, prev);
+
+        // Assert
+        expect(actual).toBe(expected);
+      }
+    );
+  });
+  describe('not yet had birthday this year', () => {
+  const testCases = [
+    { current: new Date(), prev: new Date('2001-02-29'), expected: 22 },
+    { current: new Date(), prev:new Date('2013-02-29') , expected: 10 },
+    { current: new Date(), prev: new Date('1997-02-29'), expected: 26 },
+  ];
+
+  test.each(testCases)(
+    'should calculate age difference between $current and $prev to be $expected',
+    ({ current, prev, expected }) => {
+      // Assemble
+      const sut = ageCalculator();
+
+      // Act
+      const actual = sut.calculateDifference(current, prev);
+
+      // Assert
+      expect(actual).toBe(expected);
+    }
+  );
+});
+describe('same date different year', () => {
+  const testCases = [
+    { current: new Date('2001-02-29'), prev: new Date('2011-02-29'), expected: 10 },
+    { current: new Date('1999-03-15'), prev: new Date('2009-03-15'), expected: 10 },
+    { current: new Date('1986-12-12'), prev: new Date('1997-12-12'), expected: 11 },
+  ];
+
+  test.each(testCases)(
+    'should calculate age difference between $current and $prev to be $expected',
+    ({ current, prev, expected }) => {
+      // Assemble
+      const sut = ageCalculator();
+
+      // Act
+      const actual = sut.calculateDifference(current, prev);
+
+      // Assert
+      expect(actual).toBe(expected);
+    }
+  );
+});
+
  })
